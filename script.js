e1ccdd85b7374b5e6b4060c074526d87073f3553
Khanyisa Mhlarhi <khanyisamhlarhi@gmail.com>
2022-07-14 14:40:53 +0200

    Added close button

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const handleclick =(evt)=>{
-    const menu = document.querySelector('.navbar ul')
-    if (evt.target.id==='hamId') {
-        
-        if (menu.classList.contains('menuDisplay')) {
-            menu.classList.remove('menuDisplay')
-        }else{ menu.classList.add('menuDisplay')}
-    }
-    
-    if (evt.target.id==='list') {
-        if (menu.classList.contains('menuDisplay')) {
-            menu.classList.remove('menuDisplay')
-    }
+const handleclick = (evt) => {
+  const menu = document.querySelector('.navbar ul');
+  if (evt.target.id === 'hamId') {
+    menu.classList.add('menuDisplay');
+  }
 
+  if (evt.target.id === 'list') {
+    if (menu.classList.contains('menuDisplay')) {
+      menu.classList.remove('menuDisplay');
     }
-}
+  }
 
-document.addEventListener('click', handleclick)
+  if (evt.target.id === 'closeId') {
+    if (menu.classList.contains('menuDisplay')) {
+      menu.classList.remove('menuDisplay');
+    }
+  }
+};
+
+document.addEventListener('click', handleclick);
