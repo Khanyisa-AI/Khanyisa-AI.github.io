5e5cc31956841ea2703227f1b51fb44659dc9e94
Khanyisa Mhlarhi <khanyisamhlarhi@gmail.com>
2022-07-15 07:28:58 +0200

    Change resposiveness

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+const body = document.querySelector('body');
+const hambutton = document.querySelector('.ham');
+const closebutton = document.querySelector('.close');
+
 const handleclick = (evt) => {
   const menu = document.querySelector('.navbar ul');
   if (evt.target.id === 'hamId') {
     menu.classList.add('menuDisplay');
+    body.classList.add('no-scroll');
+    hambutton.classList.toggle('hide')
+    closebutton.classList.toggle('display')
   }
 
   if (evt.target.id === 'list') {
     if (menu.classList.contains('menuDisplay')) {
+      hambutton.classList.toggle('hide',false)
       menu.classList.remove('menuDisplay');
     }
   }
@@ -13,6 +21,8 @@ const handleclick = (evt) => {
   if (evt.target.id === 'closeId') {
     if (menu.classList.contains('menuDisplay')) {
       menu.classList.remove('menuDisplay');
+      hambutton.classList.toggle('hide',false)
+      closebutton.classList.toggle('display',false)
     }
   }
 };
